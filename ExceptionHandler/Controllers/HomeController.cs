2f1fe96d78a0f42c720bc1c282ac39f5727b7a17
Akanksha Sutar <sutarakanksha1710@gmail.com>
2023-05-29 11:56:21 +0530

    first commit

---
+++ ExceptionHandler/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExceptionHandler.Filter;
+
+namespace ExceptionHandler.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [Filter.Exception]
+        [OutputCache(Duration = 60)]
+ 
+        public ActionResult Index()
+        {
+            ViewBag.CurrentTime = DateTime.Now.ToString();
+           
+            return View();
+
+        }
+        
+    }
+    
+}
+
---
+++ ExceptionHandler/Filter/Exception.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExceptionHandler.Filter
+{
+    public class Exception : FilterAttribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            if (filterContext.Exception is NotImplementedException)
+            {
+
+            }
+            else if (filterContext.Exception is DivideByZeroException)
+            {
+
+            }
+
+
+            filterContext.Result = new ViewResult()
+            {
+                ViewName = "Error"
+            };
+            filterContext.ExceptionHandled = true;
+        }
+    }
+}
